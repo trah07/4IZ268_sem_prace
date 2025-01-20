9d8204617db72d51a341f30b11e8a0f761422469
trah07 <trah07@vse.cz>
2025-01-20 03:19:03 +0100

    přidání track, mraků a skore

--- cat.js
+++ cat.js
@@ -1,12 +1,12 @@
-//board
+// board
 let board;
 let boardWidth = 750;
 let boardHeight = 250;
 let context;
 
-//kočka
+// kočka
 let catWidth = 88;
-let catHeight = 94;
+let catHeight = 72;
 let catX = 50;
 let catY = boardHeight - catHeight;
 let catImg;
@@ -18,7 +18,7 @@ let cat = {
   height: catHeight,
 };
 
-//klubíčko
+//cactus
 let cactusArray = [];
 
 let cactus1Width = 34;
@@ -34,12 +34,18 @@ let cactus2Img;
 let cactus3Img;
 
 //fyzika
-let velocityX = -8; // rychlost klubíčka směrem doleva
+let velocityX = -8; //rychlost překážek
 let velocityY = 0;
 let gravity = 0.4;
 
 let gameOver = false;
 let score = 0;
+
+// track and clouds
+let trackImg;
+let trackX = 0;
+let cloudImg;
+let clouds = [];
 
 window.onload = function () {
   board = document.getElementById("board");
@@ -48,44 +54,77 @@ window.onload = function () {
 
   context = board.getContext("2d");
 
-  //   context.fillStyle = "green";
-  //   context.fillRect(cat.x, cat.y, cat.width, cat.height);
-
   catImg = new Image();
-  catImg.src = "img/cat.png";
+  catImg.src = "./img/cat.png";
   catImg.onload = function () {
     context.drawImage(catImg, cat.x, cat.y, cat.width, cat.height);
   };
 
   cactus1Img = new Image();
-  cactus1Img.src = "img/cactus1.png";
+  cactus1Img.src = "./img/cactus1.png";
 
   cactus2Img = new Image();
-  cactus2Img.src = "img/cactus2.png";
+  cactus2Img.src = "./img/cactus2.png";
 
   cactus3Img = new Image();
-  cactus3Img.src = "img/cactus3.png";
+  cactus3Img.src = "./img/cactus3.png";
+
+  trackImg = new Image();
+  trackImg.src = "./img/track.png";
+
+  cloudImg = new Image();
+  cloudImg.src = "./img/cloud.png";
+
+  document.getElementById("game-over-container").style.display = "none";
+
+  initializeClouds();
 
   requestAnimationFrame(update);
   setInterval(placeCactus, 1000); //1000 ms
+  setInterval(placeCloud, 4000);
   document.addEventListener("keydown", moveCat);
 };
 
 function update() {
   requestAnimationFrame(update);
   if (gameOver) {
+    document.getElementById("game-over-container").style.display = "block";
+    displayFinalScore();
     return;
   }
-
   context.clearRect(0, 0, board.width, board.height);
 
-  // kočka
+  // clouds (background)
+  for (let i = 0; i < clouds.length; i++) {
+    let cloud = clouds[i];
+    cloud.x += velocityX / 4; // clouds move slowly for background effect
+    context.drawImage(cloudImg, cloud.x, cloud.y, cloud.width, cloud.height);
+  }
+
+  // Remove clouds that have moved off-screen
+  clouds = clouds.filter((cloud) => cloud.x + cloud.width > 0);
+
+  // track
+  trackX -= 4;
+  if (trackX <= -boardWidth) {
+    trackX = 0;
+  }
+  context.drawImage(trackImg, trackX, boardHeight - 30, boardWidth, 30);
+  context.drawImage(
+    trackImg,
+    trackX + boardWidth,
+    boardHeight - 30,
+    boardWidth,
+    30
+  );
+
+  //cat
   velocityY += gravity;
-  cat.y = Math.min(cat.y + velocityY, catY); // gravitace pro cat.y
+  cat.y = Math.min(cat.y + velocityY, catY); //gravitace pro cat.y
   context.drawImage(catImg, cat.x, cat.y, cat.width, cat.height);
 
-  // klubíčka
-  for (let i = 0; 1 < cactusArray.length; i++) {
+  //cactus
+  for (let i = 0; i < cactusArray.length; i++) {
     let cactus = cactusArray[i];
     cactus.x += velocityX;
     context.drawImage(
@@ -105,22 +144,23 @@ function update() {
     }
   }
 
-  // skóre
+  //score
   context.fillStyle = "black";
   context.font = "20px courier";
   score++;
-  context.filltext(score, 5, 20);
+  context.fillText(score, 5, 20);
 }
 
-// funkce skákání
 function moveCat(e) {
   if (gameOver) {
     return;
   }
 
   if ((e.code == "Space" || e.code == "ArrowUp") && cat.y == catY) {
-    // skok
+    //jump
     velocityY = -10;
+  } else if (e.code == "ArrowDown" && cat.y == catY) {
+    //duck
   }
 }
 
@@ -128,7 +168,8 @@ function placeCactus() {
   if (gameOver) {
     return;
   }
-  //vkladání klubíčka
+
+  //place cactus
   let cactus = {
     img: null,
     x: cactusX,
@@ -138,34 +179,84 @@ function placeCactus() {
   };
 
   let placeCactusChance = Math.random();
+
   if (placeCactusChance > 0.9) {
-    // 10% šance pro cactus3
+    //10% you get cactus3
     cactus.img = cactus3Img;
     cactus.width = cactus3Width;
     cactusArray.push(cactus);
   } else if (placeCactusChance > 0.7) {
-    // 10% šance pro cactus2
+    //30% you get cactus2
     cactus.img = cactus2Img;
     cactus.width = cactus2Width;
     cactusArray.push(cactus);
-  }
-  if (placeCactusChance > 0.5) {
-    // 50% šance pro cactus1
+  } else if (placeCactusChance > 0.5) {
+    //50% you get cactus1
     cactus.img = cactus1Img;
     cactus.width = cactus1Width;
     cactusArray.push(cactus);
   }
 
   if (cactusArray.length > 5) {
-    cactusArray.shift(); // podmínka, aby nebylo víc než 5 kaktusů (kontrola paměti)
+    cactusArray.shift(); // zabránění zvětšní pole
   }
+}
+
+function placeCloud() {
+  if (gameOver) {
+    return;
+  }
+
+  let cloud = {
+    x: boardWidth,
+    y: Math.random() * (boardHeight / 3), // Random height in upper third of the canvas
+    width: 80,
+    height: 50,
+  };
+  clouds.push(cloud);
+}
+
+function initializeClouds() {
+  clouds = [];
+  for (let i = 0; i < 3; i++) {
+    placeInitialCloud(i * 250 + 100);
+  }
+}
+
+function placeInitialCloud(offset) {
+  let cloud = {
+    x: offset,
+    y: Math.random() * (boardHeight / 3),
+    width: 80,
+    height: 50,
+  };
+  clouds.push(cloud);
 }
 
 function detectCollision(a, b) {
   return (
-    a.x < b.x + b.width && // levý horní roh a nedosáhne na pravý horní roh b
-    a.x + a.width > b.x && // levý horní roh a přejde přes pravý horní roh b
-    a.y < b.y + b.height && // levý spodní roh a nedosáhne na pravý spodní roh b
-    a.y + a.height > b.y // levý spodní roh a přesáhne pravý spodní roh b
-  );
+    a.x < b.x + b.width && //levý horní roh a nedosahuje pravého horního rohu b
+    a.x + a.width > b.x && //pravý horní roh a přechází do levého horního rohu b
+    a.y < b.y + b.height && //levý horní roh a nedosahuje levého dolního rohu b
+    a.y + a.height > b.y
+  ); //levý dolní roh a prochází levým horním rohem b
+}
+
+function displayFinalScore() {
+  const scoreContainer = document.getElementById("score-container");
+  const finalScore = document.getElementById("final-score");
+  finalScore.textContent = `Finalní skóre: ${score}`;
+  scoreContainer.style.display = "block";
+}
+
+function restartGame() {
+  gameOver = false;
+  score = 0;
+  velocityY = 0;
+  cat.y = catY;
+  catImg.src = "./img/cat.png";
+  cactusArray = [];
+  initializeClouds();
+  document.getElementById("game-over-container").style.display = "none";
+  scoreContainer.style.display = "none";
 }
